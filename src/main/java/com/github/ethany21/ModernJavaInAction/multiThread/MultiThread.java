d440d72b9540d9c7b41f09daf518fc19455269b7
ethany21 <imwoodam@hanmail.net>
2021-06-08 00:17:07 +0900

    Added example for Thread join

--- src/main/java/com/github/ethany21/ModernJavaInAction/multiThread/MultiThread.java
+++ src/main/java/com/github/ethany21/ModernJavaInAction/multiThread/MultiThread.java
@@ -1,13 +1,40 @@
 package com.github.ethany21.ModernJavaInAction.multiThread;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MultiThread {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        Thread thread = new Thread(new BlockingTask());
+        List<Long> inputNumbers = Arrays.asList(0L, 34L, 25L, 231L, 556L);
 
-        thread.start();
-        thread.interrupt();
+        List<FactorialThread> threads = new ArrayList<>();
+
+        for(long inputNumber : inputNumbers){
+            threads.add(new FactorialThread(inputNumber));
+        }
+
+        for (Thread thread : threads){
+            thread.start();
+        }
+
+        for (Thread thread : threads){
+            thread.join();
+        }
+
+
+        for (int i = 0; i < inputNumbers.size(); i++){
+
+            FactorialThread factorialThread = threads.get(i);
+            if(factorialThread.isFinished()){
+                System.out.println("Factorial of " + inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println("The calculation of " + inputNumbers.get(i) + " is still running");
+            }
+
+        }
 
     }
 
